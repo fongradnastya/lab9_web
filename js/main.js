2e4b6093c3df6c49b7d75f68a07abae081d188a7
Anastasia Fongrad <fongradnastya@gmail.com>
2023-03-15 22:42:03 +0700

    Added common price counting

--- js/main.js
+++ js/main.js
@@ -1,38 +1,5 @@
-// После загрузки страницы вывести в консоль информацию о всех
-// ссылках, якорях, изображениях на странице.
-
-// И изображения, и ссылки
-let allLinks = Array.from(document.getElementsByTagName("a"));
-
-let allImages = Array.from(document.getElementsByTagName("img"));
-
-console.log('Links and anchors:')
-allLinks.forEach((item, index) => {
-    console.log(`Link id: ${index}: <a href=${item.href}>${item.innerText}</a>`);
-})
-
-console.log('Images:')
-allImages.forEach((item, index) => {
-    console.log(`Image id: ${index}: <img src=${item.src} alt=${item.alt}>`);
-})
-
-// Обработчики событий
-
-const carouselPrev = document.querySelector('.carousel-control-prev'); // Нельзя лобавить несколько обработчиков
-const carouselNext = document.querySelector('.carousel-control-next');
-carouselPrev.onclick = () => { console.log("Shown previous page") };
-carouselNext.onclick = () => { console.log("Shown next page") };
-
-let limitedImg = document.querySelector('.swap');
-limitedImg.onmouseover = () => { console.log("Your mouse is over the picture ") };
-
-let header = document.getElementsByClassName("header__title");
-header[0].addEventListener("mousedown", (e) => { console.log(e); }, false);
-let logo = document.getElementsByClassName("logo");
-logo[0].addEventListener("mouseout", (e) => { console.log(e); }, false);
-
 // Анимация
-
+let limitedImg = document.querySelector('.swap');
 let index = 0;
 let imgs = Array("img/limited-edition/photo1.png", "img/limited-edition/photo2.png",
     "img/limited-edition/photo3.png");
@@ -49,4 +16,27 @@ function swapImages() {
     }
 }
 
-limitedImg.addEventListener("click", swapImages, false)
+limitedImg.addEventListener("click", swapImages, false);
+
+
+// Работа с корзиной
+
+const basketPrise = document.querySelector(".header__prise");
+const productsAdd = document.querySelectorAll(".popular__link");
+console.log(basketPrise.textContent);
+basketPrise.textContent = 0 + " р.";
+
+let commonPrice = 0;
+
+productsAdd.forEach((item)=>{
+    item.addEventListener("click", function (event) {
+        console.log(1);
+        const card = event.target.closest(".popular__card");
+        const productPrice = card.querySelector(".popular__price");
+        let price = Number(productPrice.textContent.trim().slice(0, -3));
+        commonPrice += price;
+        basketPrise.textContent = commonPrice + " р.";
+        console.log(price);
+    })
+})
+
